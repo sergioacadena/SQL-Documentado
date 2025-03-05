23ff3bde589be80a313ffc200c27f2b4912eb948
Sergio A Cadena <sergio.cadena@unicab.org>
2025-03-05 10:24:30 -0500

    commit unions

--- 05_join_BD/04_union.sql
+++ 05_join_BD/04_union.sql
@@ -6,3 +6,24 @@ SELECT * FROM users
 FULL JOIN cedula
 ON users.user_id = cedula.user_id;
 
+/* UNION */
+/* me trae los resultados de las dos tablas, pero no me trae los repetidos
+Recupera los valores de user_id desde ambas tablas (users y cedula).
+Elimina duplicados, porque UNION por defecto solo devuelve valores únicos.
+Une los resultados de ambas tablas en una sola columna.
+ */
+SELECT user_id FROM users 
+UNION
+SELECT user_id FROM cedula
+
+/* UNION ALL */
+/* me trae los resultados de las dos tablas, pero me trae los repetidos
+Recupera los valores de user_id desde ambas tablas (users y cedula).
+No elimina duplicados, porque UNION ALL devuelve todos los valores.
+Une los resultados de ambas tablas en una sola columna.
+ */
+SELECT user_id FROM users
+UNION ALL   
+SELECT user_id FROM cedula
+
+
